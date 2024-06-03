ef6f083856dea879968cfb451ea3051498cedf7e
dvz <79332655+KsR4z8v@users.noreply.github.com>
2024-06-03 17:15:35 -0500

    improved logic and refactor

--- src/MinMax.py
+++ src/MinMax.py
@@ -1,68 +1,91 @@
 import numpy as np
 from Node import Node
 import copy
-
+from tabulate import tabulate
 from os import system
 
 
-class Chess:
-    def __init__(self) -> None:
-        self.globalGame = [
-            ['T1', 'C1', 'A1', '  ', 'N1', '  ', 'C1', 'T1'],
-            ['P1', '  ', '  ', '  ', 'R1', '  ', 'P1', 'P1'],
-            ['P1', '  ', 'P1', 'P1', '  ', '  ', '  ', '  '],
-            ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
-            ['  ', 'P2', '  ', '  ', '  ', '  ', '  ', '  '],
-            ['  ', '  ', '  ', '  ', '  ', '  ', 'A1', '  '],
-            ['  ', '  ', 'P2', 'P2', '  ', '  ', '  ', '  '],
-            ['T2', 'C2', 'A2', 'R2', 'N2', '  ', '  ', 'T2'],
-        ]
+testGamge = [
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', 'C1', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+    ['P2', 'P2', 'P2', 'P2', 'P2', 'P2', 'P2', 'P2'],
+    ['T2', 'C2', 'A2', 'R2', 'N2', 'A2', 'C2', 'T2'],
+]
 
-        self.humanPlayer = 1
-        self.pcPlayer = 2
-        self.deep_limit = 2
+
+class Chess:
+    def __init__(self, humanPlayer) -> None:
+        self.globalGame = [
+            ['T1', 'C1', 'A1', 'R1', 'N1', 'A1', 'C1', 'T1'],
+            ['P1', 'P1', 'P1', 'P1', 'P1', 'P1', 'P1', 'P1'],
+            ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+            ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+            ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+            ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+            ['P2', 'P2', 'P2', 'P2', 'P2', 'P2', 'P2', 'P2'],
+            ['T2', 'C2', 'A2', 'R2', 'N2', 'A2', 'C2', 'T2'],
+        ]
+        # -1=MIN , 1=MAX
+        self.humanPlayer = -1 if humanPlayer == 2 else 1
+        self.deep_limit = 4
 
     def run(self):
-        tree = self.generateTree()
-        minimax = self.calculateMinimax(tree)
-        self.globalGame = copy.deepcopy(minimax.game)
-        print(minimax.type, minimax.utility, minimax.deep)
-        self.printGame(self.globalGame)
+        while True:
+            system('cls')
+            self.printGame(self.globalGame)
+            yf, xf = input('ingrese la ficha a mover y,x: ').split(',')
+            yf = int(yf)
+            xf = int(xf)
+            selected = self.globalGame[yf][xf]
+            yd, xd = input('ingrese la posicion destino: ').split(',')
+            yd = int(yd)
+            xd = int(xd)
+            self.globalGame[yd][xd] = selected
+            self.globalGame[yf][xf] = '  '
+
+            tree = self.generateTree()  # genero el arbol de juego
+            node: Node = self.calculateMinimax(
+                tree)  # obtengo la jugada minimax
+
+            # remplazo el juego actual con el de la jugada minimax
+            self.globalGame = copy.deepcopy(node.minimax)
+
+            node = Node(self.globalGame, None, None, None, 0)
+            node.isLeft()
+
+            system('cls')
+            if node.p1 == 0:
+                self.printGame(self.globalGame)
+                print('Gana jugador 1')
+                break
+            if node.p2 == 0:
+                print('Gana jugador 2')
+                self.printGame(self.globalGame)
+                break
 
     def calculateMinimax(self, tree: list):
-        maxDeep = self.deep_limit
-        while (len(tree) != 0 and maxDeep > 0):
+        while len(tree) != 1:
 
-            nodeMaxDeep: Node = None
-
-            for i in range(0, len(tree)):
-                if tree[i].deep == maxDeep:
-                    nodeMaxDeep = tree.pop(i)
-                    break
-
-            if (nodeMaxDeep == None):
-                maxDeep -= 1
-                continue
-
+            nodeMaxDeep: Node = tree.pop()
             parent: Node = nodeMaxDeep.parent
-            """             if nodeMaxDeep.utility != 0:
-                print(f''''UTILIDAD:{nodeMaxDeep.utility} TIPO: {
-                    nodeMaxDeep.type} PROFUNDIDAD: {nodeMaxDeep.deep}''''')
-                self.printGame(nodeMaxDeep.game)
-                system('pause') """
-            if (parent.type == 1):
-                if (nodeMaxDeep.utility > parent.utility):
-                    parent.utility = nodeMaxDeep.utility
-                    parent.minimax = nodeMaxDeep
-            else:
-                if (nodeMaxDeep.utility < parent.utility):
-                    parent.utility = nodeMaxDeep.utility
-                    parent.minimax = nodeMaxDeep
 
-        return tree[0].minimax
+            if (parent.type == 1):
+                if (nodeMaxDeep.utility >= parent.utility):
+                    parent.utility = nodeMaxDeep.utility
+                    parent.minimax = nodeMaxDeep.game
+            else:
+                if (nodeMaxDeep.utility <= parent.utility):
+                    parent.utility = nodeMaxDeep.utility
+                    parent.minimax = nodeMaxDeep.game
+
+        return tree[0]
 
     def generateTree(self):
-
+        # auxiliares
         def generateChild(parent: Node, f, i, j, xi, yi):
             game = copy.deepcopy(parent.game)
             game[yi][xi] = f
@@ -72,19 +95,19 @@ class Chess:
             utility = float(
                 '-inf') if type == 1 else float('inf')
 
-            """ if (parent.deep+1 == 1):
-                self.printGame(game)
-                system('pause') """
             return Node(game, parent, type, utility, parent.deep+1)
 
+        # nodo inicial del juego actual / por defecto la maquina juega minimizando, player1 = MAX player2 = MIN
         initNode = Node(copy.deepcopy(self.globalGame),
-                        None, -1, float('inf'), 0)
+                        None, self.humanPlayer*-1, self.humanPlayer*float('inf'), 0)
 
-        queue = [initNode]
+        queue = [initNode]  # generar los hijos por amplitud
         tree = []
 
         while len(queue) != 0:
+
             currentNode: Node = queue.pop(0)
+
             tree.append(currentNode)
             # expando el nodo para saber si es un nodo hoja o si ya alcanzo el limite establecido
             if (currentNode.isLeft() or currentNode.deep == self.deep_limit):
@@ -117,7 +140,7 @@ class Chess:
                             moves.append((generateChild(
                                 currentNode, ficha, i, j, xi, yi), False))
 
-                            if currentNode.game[yi-direction][xi] == '  ' and (i == 6 or i == 1):
+                            if yi-direction >= 0 and yi-direction <= 7 and currentNode.game[yi-direction][xi] == '  ' and (i == 6 or i == 1):
                                 moves.append((generateChild(
                                     currentNode, ficha, i, j, xi, yi - direction), False))
 
@@ -151,7 +174,8 @@ class Chess:
                                 moves.append((generateChild(
                                     currentNode, ficha, i, j, xi, yi), isCapture))
 
-                    if (ficha == f'A{player}'):  # Alfir
+                    # Alfir o Reina // DIAGONALES
+                    if (ficha == f'A{player}' or ficha == f'N{player}'):
                         movements = []
                         for k in range(1, 8):  # diagonal superior derecha
                             ya, xa = (i-k, j+k)
@@ -198,24 +222,69 @@ class Chess:
                                 moves.append((generateChild(
                                     currentNode, ficha, i, j, xi, yi), isCapture))
 
-            # si hubo un nodo donde caputura alguna pieza filtro las que no, esto con el fin de obligarlo a capturar
+                    # Torre o Reina // verticales y horizontales
+                    if (ficha == f'T{player}' or ficha == f'N{player}'):
+                        movements = []
+                        for k in range(1, 8):  # arriba
+                            ya, xa = (i-k, j)
+                            if (ya <= 7 and ya >= 0 and xa >= 0 and xa <= 7):
+                                if currentNode.game[ya][xa] != '  ':
+                                    movements.append((ya, xa))
+                                    break
+                                else:
+                                    movements.append((ya, xa))
+
+                        for k in range(1, 8):  # abajo
+                            ya, xa = (i+k, j)
+                            if (ya <= 7 and ya >= 0 and xa >= 0 and xa <= 7):
+                                if currentNode.game[ya][xa] != '  ':
+                                    movements.append((ya, xa))
+                                    break
+                                else:
+                                    movements.append((ya, xa))
+                        for k in range(1, 8):  # derecha
+                            ya, xa = (i, j+k)
+                            if (ya <= 7 and ya >= 0 and xa >= 0 and xa <= 7):
+                                if currentNode.game[ya][xa] != '  ':
+                                    movements.append((ya, xa))
+                                    break
+                                else:
+                                    movements.append((ya, xa))
+                        for k in range(1, 8):  # izquierda
+                            ya, xa = (i, j-k)
+                            if (ya <= 7 and ya >= 0 and xa >= 0 and xa <= 7):
+                                if currentNode.game[ya][xa] != '  ':
+                                    movements.append((ya, xa))
+                                    break
+                                else:
+                                    movements.append((ya, xa))
+
+                        for yi, xi in movements:
+                            if yi <= 7 and yi >= 0 and xi >= 0 and xi <= 7:
+                                if currentNode.game[yi][xi][1] == str(player):
+                                    continue
+                                isCapture = False
+                                if (currentNode.game[yi][xi][1] != str(player) and currentNode.game[yi][xi] != '  '):
+                                    isCapture = True
+                                    hasCaptureMovements = True
+                                moves.append((generateChild(
+                                    currentNode, ficha, i, j, xi, yi), isCapture))
+
+            # si hubo un nodo donde caputura alguna pieza descarto las que no, esto con el fin de obligarlo a capturar
             for child, wasCapture in moves:
                 if wasCapture and hasCaptureMovements:
                     queue.append(child)
                     continue
                 if not hasCaptureMovements:
                     queue.append(child)
-
         return tree
 
     def printGame(self, game):
         for i in range(0, 8):
-            print(i, end=' | ')
-            for j in range(0, 8):
-                print(game[i][j], end='')
-
-            print()
+            print(' '*2, i, end=' ')
+        print()
+        print(tabulate(game, tablefmt="simple_grid"))
 
 
-chess = Chess()
+chess = Chess(1)
 chess.run()
--- src/Node.py
+++ src/Node.py
@@ -6,10 +6,12 @@ class Node:
         self.deep = deep
         self.utility = utility
         self.minimax: Node = None  # guardo el nodo de la desicion minimax tomada
-        self.p1 = 0  # total de ficas de player 1
-        self.p2 = 0  # total de fichas de player 2
+        self.p1 = 0  # Puntaje jugador 1 // total de fichas de player 1
+        self.p2 = 0  # Puntaje jugador 2 // total de fichas de player 2
 
-    def isLeft(self):  # se calcula que pasa en el nodo
+    def isLeft(self):  # se calcula que pasa en el nodo y se guarda el puntaje de cada jugador que seria el numero de fichas de cada uno
+        self.p1 = 0
+        self.p2 = 0
         for i in range(0, 8):
             for j in range(0, 8):
                 if (self.game[i][j][1] == '1'):
@@ -20,8 +22,17 @@ class Node:
 
     def calculateUtility(self):
         if (self.type == 1):  # en caso de que sea un nodo tipo MAX
-            self.utility = 16-self.p1
+            if self.p2 == 0:
+                self.utility = float('-inf')
+            elif self.p1 == 0:
+                self.utility = float('inf')
+            else:
+                self.utility = 16-self.p1
         else:  # en caso de que sea un nodo tipo MIN
-            self.utility = -(16-self.p2)
-
+            if self.p1 == 0:
+                self.utility = float('-inf')
+            elif self.p2 == 0:
+                self.utility = float('inf')
+            else:
+                self.utility = -(16-self.p2)
         return self.utility
